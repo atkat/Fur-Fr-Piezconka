0fc464f8130e3491c04d050b35799d98a404afe0
atkat <51533188+atkat@users.noreply.github.com>
2021-01-03 23:41:00 +0100

    files

---
+++ Jobcenter.js
@@ -0,0 +1,32 @@
+
+
+let counter = -1
+
+$(window).on("click", function(event){
+    event.preventDefault();
+        counter ++
+
+        Img(images[counter])
+        .appendTo("body")
+        .css("top", event.pageY - 50 +"px")
+        .css("left", event.pageX - 200 + "px")  
+            
+})
+
+var images = [
+    'popups/Frauenquote.jpeg',
+    'popups/Neue-Jobs.jpeg',
+    'popups/Praktisch.jpeg',
+    'popups/Vegleichbar.jpeg',
+    'popups/erfolsweg.png',
+    'popups/Schaffen.jpeg'
+]
+
+
+function Img (str) {
+    return $("<img>")
+        .attr("src", str)
+        .attr("width", "400")
+        .css("position", "absolute")
+
+}
